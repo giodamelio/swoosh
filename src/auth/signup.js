9548526c991e64c5dcbbbb7c23339e19b5d7fbf8
Gio d'Amelio <giodamelio@gmail.com>
2019-08-19 21:41:22 +0000

    Fresh start

--- src/auth/signup.js
+++
@@ -1,7 +0,0 @@
-module.exports.handler = async (event, context) => {
-  console.log('Event', event);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello World' })
-  };
-};
